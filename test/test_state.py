f7bffa78c48f3c95ff1ef3d197f68ad910a7976d
crazynayan <nayan@crazyideas.co.in>
2019-09-05 16:48:56 +0530

    Register with Mask updates

--- test/test_state.py
+++ test/test_state.py
@@ -15,6 +15,11 @@ class StateTest(unittest.TestCase):
         self.assertEqual(bytearray([0xFF, 0xFF, 0xFF, 0xFF]), self.regs.R2)
         self.assertRaises(AttributeError, self.regs.get_value, 'RAC')
         self.assertRaises(AttributeError, self.regs.set_value, '0', 0)
+        self.regs.set_bytes('R3', bytearray([0x12, 0x34, 0x56, 0x78]))
+        self.assertEqual(bytearray([0x12, 0x34, 0x56, 0x78]), self.regs.R3)
+        self.assertEqual(bytearray([0x12, 0x56]), self.regs.get_bytes_from_mask('R3', 10))
+        self.regs.set_bytes_from_mask('R3', bytearray([0x85, 0x23]), 10)
+        self.assertEqual(bytearray([0x85, 0x34, 0x23, 0x78]), self.regs.get_bytes('R3'))
 
 
 if __name__ == '__main__':
--- v2/state.py
+++ v2/state.py
@@ -1,21 +1,24 @@
 class Registers:
+    ORDER = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15']
+    LEN = 4
+
     def __init__(self):
-        self.R0 = bytearray([0x00] * 4)
-        self.R1 = bytearray([0x00] * 4)
-        self.R2 = bytearray([0x00] * 4)
-        self.R3 = bytearray([0x00] * 4)
-        self.R4 = bytearray([0x00] * 4)
-        self.R5 = bytearray([0x00] * 4)
-        self.R6 = bytearray([0x00] * 4)
-        self.R7 = bytearray([0x00] * 4)
-        self.R8 = bytearray([0x00] * 4)
-        self.R9 = bytearray([0x00] * 4)
-        self.R10 = bytearray([0x00] * 4)
-        self.R11 = bytearray([0x00] * 4)
-        self.R12 = bytearray([0x00] * 4)
-        self.R13 = bytearray([0x00] * 4)
-        self.R14 = bytearray([0x00] * 4)
-        self.R15 = bytearray([0x00] * 4)
+        self.R0 = bytearray([0x00] * self.LEN)
+        self.R1 = bytearray([0x00] * self.LEN)
+        self.R2 = bytearray([0x00] * self.LEN)
+        self.R3 = bytearray([0x00] * self.LEN)
+        self.R4 = bytearray([0x00] * self.LEN)
+        self.R5 = bytearray([0x00] * self.LEN)
+        self.R6 = bytearray([0x00] * self.LEN)
+        self.R7 = bytearray([0x00] * self.LEN)
+        self.R8 = bytearray([0x00] * self.LEN)
+        self.R9 = bytearray([0x00] * self.LEN)
+        self.R10 = bytearray([0x00] * self.LEN)
+        self.R11 = bytearray([0x00] * self.LEN)
+        self.R12 = bytearray([0x00] * self.LEN)
+        self.R13 = bytearray([0x00] * self.LEN)
+        self.R14 = bytearray([0x00] * self.LEN)
+        self.R15 = bytearray([0x00] * self.LEN)
 
     def get_value(self, reg):
         return int.from_bytes(getattr(self, reg), 'big', signed=True)
@@ -24,3 +27,22 @@ class Registers:
         getattr(self, reg)
         setattr(self, reg, bytearray(value.to_bytes(4, 'big', signed=True)))
 
+    def next_reg(self, reg):
+        getattr(self, reg)
+        return self.ORDER[0] if reg == self.ORDER[-1] else self.ORDER[self.ORDER.index(reg) + 1]
+
+    def get_bytes(self, reg):
+        return getattr(self, reg)
+
+    def get_bytes_from_mask(self, reg, mask):
+        reg_bytes = getattr(self, reg)
+        return bytearray([reg_bytes[index] for index, bit in enumerate(bin(mask)[2:]) if bit == '1'])
+
+    def set_bytes(self, reg, byte_array):
+        getattr(self, reg)
+        setattr(self, reg, byte_array[:self.LEN])
+
+    def set_bytes_from_mask(self, reg, byte_array, mask):
+        reg_bytes = getattr(self, reg)
+        reg_bytes = [byte_array.pop(0) if bit == '1' else reg_bytes[index] for index, bit in enumerate(bin(mask)[2:])]
+        setattr(self, reg, bytearray(reg_bytes))
