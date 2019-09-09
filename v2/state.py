fb226a938b22c89c094d15896fe29062304cc0e9
crazynayan <nayan@crazyideas.co.in>
2019-09-09 19:08:45 +0530

    Storage Test

--- v2/state.py
+++ v2/state.py
@@ -6,59 +6,56 @@ class Registers:
     LEN = 4
 
     def __init__(self):
-        self.R0 = bytearray([0x00] * self.LEN)
-        self.R1 = bytearray([0x00] * self.LEN)
-        self.R2 = bytearray([0x00] * self.LEN)
-        self.R3 = bytearray([0x00] * self.LEN)
-        self.R4 = bytearray([0x00] * self.LEN)
-        self.R5 = bytearray([0x00] * self.LEN)
-        self.R6 = bytearray([0x00] * self.LEN)
-        self.R7 = bytearray([0x00] * self.LEN)
-        self.R8 = bytearray([0x00] * self.LEN)
-        self.R9 = bytearray([0x00] * self.LEN)
-        self.R10 = bytearray([0x00] * self.LEN)
-        self.R11 = bytearray([0x00] * self.LEN)
-        self.R12 = bytearray([0x00] * self.LEN)
-        self.R13 = bytearray([0x00] * self.LEN)
-        self.R14 = bytearray([0x00] * self.LEN)
-        self.R15 = bytearray([0x00] * self.LEN)
+        self.R0 = 0
+        self.R1 = 0
+        self.R2 = 0
+        self.R3 = 0
+        self.R4 = 0
+        self.R5 = 0
+        self.R6 = 0
+        self.R7 = 0
+        self.R8 = 0
+        self.R9 = 0
+        self.R10 = 0
+        self.R11 = 0
+        self.R12 = 0
+        self.R13 = 0
+        self.R14 = 0
+        self.R15 = 0
 
     def get_bytes(self, reg):
-        try:
-            return getattr(self, reg)
-        except AttributeError:
-            raise AttributeError
+        return DataType('F', input=str(self.get_value(reg))).to_bytes(self.LEN)
 
     def get_value(self, reg):
-        return DataType('F', bytes=self.get_bytes(reg)).value
-
-    def get_address(self, base, dsp, index=None):
         try:
-            return (self.get_value(base.reg) if base.reg != 'R0' else 0) + dsp + \
-                   (self.get_value(index.reg) if index and index.reg != 'R0' else 0)
+            return getattr(self, str(reg))
         except AttributeError:
             raise AttributeError
 
+    def get_address(self, base, dsp, index=None):
+        return (self.get_value(str(base)) if str(base) != 'R0' else 0) + dsp + \
+               (self.get_value(str(index)) if index and str(index) != 'R0' else 0)
+
     def next_reg(self, reg):
-        self.get_bytes(reg)
+        self.get_value(reg)
         return self.ORDER[0] if reg == self.ORDER[-1] else self.ORDER[self.ORDER.index(reg) + 1]
 
     def get_bytes_from_mask(self, reg, mask):
         reg_bytes = self.get_bytes(reg)
         try:
-            return bytearray([reg_bytes[index] for index, bit in enumerate(bin(mask)[2:]) if bit == '1'])
+            return bytearray([reg_bytes[index] for index, bit in enumerate(f'{mask:04b}') if bit == '1'])
         except IndexError:
             raise IndexError
 
     def set_bytes(self, byte_array, reg):
-        self.get_bytes(reg)
+        self.get_value(reg)
         if len(byte_array) != self.LEN:
             raise ValueError
-        setattr(self, reg, byte_array[:])
+        setattr(self, str(reg), DataType('F', bytes=byte_array).value)
 
     def set_value(self, value, reg):
-        self.get_bytes(reg)
-        setattr(self, reg, DataType('F', input=str(value)).to_bytes())
+        self.get_value(reg)
+        setattr(self, str(reg), value)
 
     def set_bytes_from_mask(self,  byte_array, reg, mask):
         reg_bytes = self.get_bytes(reg)
@@ -66,29 +63,40 @@ class Registers:
             raise ValueError
         try:
             reg_bytes = [byte_array.pop(0) if bit == '1' else reg_bytes[index]
-                         for index, bit in enumerate(bin(mask)[2:])]
+                         for index, bit in enumerate(f'{mask:04b}')]
         except IndexError:
             raise IndexError
-        setattr(self, reg, bytearray(reg_bytes))
+        setattr(self, str(reg), DataType('F', bytes=bytearray(reg_bytes)).value)
 
 
 class Storage:
-    F4K = 4096
+    F4K = 0x00001000
     ECB = F4K * 1
-    GLOBAL = F4K * 2
-    # Space for 14 more fixed frames
+    GLOBAL = F4K * 2     # 14 more fixed frames are spare
+    GLOBAL_FRAME_SIZE = 1 << 4
     NIBBLE = 4
     NIBBLE_3 = 12
+    ZERO = 0x00
+    ONES = 0xFF
 
     def __init__(self):
-        self.frames = dict()
-        self.frames[self.base_key(self.ECB)] = bytearray([0x00] * self.F4K)
-        self.frames[self.base_key(self.GLOBAL)] = bytearray([0x00] * self.F4K)
-        self.nab = self.F4K << self.NIBBLE
+        self.frames = dict()                # Frames init with ZERO
+        self._frame = dict()               # Frames init with ONES
+        self.nab = self.F4K << self.NIBBLE  # To ensure total 16 fixed frames
+        self.frames[self.base_key(self.ECB)] = bytearray([self.ZERO] * self.F4K)
+        self.frames[self.base_key(self.GLOBAL)] = bytearray([self.ZERO] * self.GLOBAL_FRAME_SIZE)
+        self._frame[self.base_key(self.ECB)] = bytearray([self.ONES] * self.F4K)
+        self._frame[self.base_key(self.GLOBAL)] = bytearray([self.ONES] * self.GLOBAL_FRAME_SIZE)
 
     def allocate(self):
-        self.frames[self.base_key(self.nab)] = bytearray()
+        base_address = self.base_key(self.nab)
+        self.frames[base_address] = bytearray()
+        self._frame[base_address] = bytearray()
         self.nab += self.F4K
+        return self.nab - self.F4K
+
+    def get_allocated_address(self):
+        return DataType('F', input=str(self.nab - self.F4K)).to_bytes(Registers.LEN)
 
     def base_key(self, address):
         address = (address >> self.NIBBLE_3) << self.NIBBLE_3
@@ -97,26 +105,67 @@ class Storage:
     def dsp(self, address):
         return address & (self.F4K - 1)
 
-    def _get_data(self, address, length=None):
-        base_address = self.base_key(address)
+    def extend_frame(self, base_address, length):
         try:
-            data = self.frames[base_address]
+            frame_len = len(self.frames[base_address])
         except KeyError:
             raise KeyError
+        if length > frame_len:
+            self.frames[base_address].extend(bytearray([self.ZERO] * (length - frame_len)))
+            self._frame[base_address].extend(bytearray([self.ONES] * (length - frame_len)))
+
+    def _get_data(self, address, length=None):
+        base_address = self.base_key(address)
         start = self.dsp(address)
         end = start + 1 if length is None else start + length
-        if end > len(data):
-            data.extend(bytearray([0x00] * (end - len(data))))
+        self.extend_frame(base_address, end)
         return base_address, start, end
 
     def get_bytes(self, address, length=None):
         base_address, start, end = self._get_data(address, length)
         return self.frames[base_address][start: end]
 
-    def get_value(self, address, length=None):
-        return DataType('X', bytes=self.get_bytes(address, length)).value
+    def get_value(self, address, length=4):
+        return DataType('F', bytes=self.get_bytes(address, length)).value
 
     def set_bytes(self, byte_array, address, length=None):
         base_address, start, end = self._get_data(address, length)
         for index, byte in enumerate(byte_array[: end - start]):
             self.frames[base_address][index + start] = byte
+            self._frame[base_address][index + start] = byte
+
+    def set_value(self, value, address, length=4):
+        base_address, start, end = self._get_data(address, length)
+        for index, byte in enumerate(DataType('F', input=str(value)).to_bytes(end - start)):
+            self.frames[base_address][index + start] = byte
+            self._frame[base_address][index + start] = byte
+
+    def is_updated(self, address, length=None):
+        # Will return True only if all requested bytes are updated
+        base_address, start, end = self._get_data(address, length)
+        return self.frames[base_address][start: end] == self._frame[base_address][start: end]
+
+    def all_bits_on(self, address, bits):
+        # Will return True only if all requested bits are ON
+        base_address, dsp, _ = self._get_data(address)
+        return self.frames[base_address][dsp] & bits == bits
+
+    def all_bits_off(self, address, bits):
+        # Will return True only if all requested bits are OFF
+        base_address, dsp, _ = self._get_data(address)
+        return self.frames[base_address][dsp] & bits == 0
+
+    def set_bit_on(self, address, bit):
+        base_address, dsp, _ = self._get_data(address)
+        self.frames[base_address][dsp] |= bit
+        self._frame[base_address][dsp] |= bit
+
+    def set_bit_off(self, address, bit):
+        base_address, dsp, _ = self._get_data(address)
+        self.frames[base_address][dsp] = self.frames[base_address][dsp] & ~bit
+        self._frame[base_address][dsp] = self._frame[base_address][dsp] & ~bit
+
+    def is_updated_bit(self, address, bit):
+        # Will return True only if all requested bits are updated
+        base_address, dsp, _ = self._get_data(address)
+        return self.frames[base_address][dsp] & bit == self._frame[base_address][dsp] & bit
