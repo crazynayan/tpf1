a548b7040ef55429ef2d9c4f2c754fcb99f067af
nayan <nayan@crazyideas.co.in>
2021-04-27 17:27:26 +0530

    DBCLS REF=ALL

--- p4_execution/ex4_db_macro.py
+++ p4_execution/ex4_db_macro.py
@@ -15,14 +15,14 @@ from p4_execution.ex1_state import State
 
 class UserDefinedDbMacro(State):
     def _pd0_base(self, node: KeyValue) -> Optional[int]:
-        workarea = node.get_value('WORKAREA')
+        workarea = node.get_value("WORKAREA")
         if workarea is None:
             raise Pd0BaseError
-        if workarea[0] == 'LEV':
+        if workarea[0] == "LEV":
             level = f"D{workarea[1]}" if len(workarea[1]) == 1 else workarea[1]
-            level_address = self.get_ecb_address(level, 'CE1CR')
+            level_address = self.get_ecb_address(level, "CE1CR")
             pd0_base = self.vm.get_value(level_address)
-        elif workarea[0] == 'REG':
+        elif workarea[0] == "REG":
             reg = Register(workarea[1])
             if not reg.is_valid():
                 raise RegisterInvalidError
@@ -32,9 +32,9 @@ class UserDefinedDbMacro(State):
         return pd0_base
 
     def _get_pnr_locator(self):
-        aaa_address = self.vm.get_value(self.regs.R9 + self.seg.evaluate('CE1CR1'))
-        self.seg.load_macro('WA0AA')
-        pnr_ordinal = self.vm.get_value(aaa_address + self.seg.evaluate('WA0PWR'))
+        aaa_address = self.vm.get_value(self.regs.R9 + self.seg.evaluate("CE1CR1"))
+        self.seg.load_macro("WA0AA")
+        pnr_ordinal = self.vm.get_value(aaa_address + self.seg.evaluate("WA0PWR"))
         return PnrLocator.to_locator(pnr_ordinal)
 
     def pdcls(self, node: KeyValue) -> str:
@@ -44,8 +44,8 @@ class UserDefinedDbMacro(State):
 
     def pdred(self, node: KeyValue) -> str:
         # Get the key from FIELD= or INDEX=
-        self.seg.load_macro('PDEQU')
-        self.seg.load_macro('PD0WRK')
+        self.seg.load_macro("PDEQU")
+        self.seg.load_macro("PD0WRK")
         key_label = f"#PD_{node.get_value('FIELD')}_K"
         try:
             key_number = self.seg.evaluate(key_label)
@@ -55,15 +55,15 @@ class UserDefinedDbMacro(State):
             raise PdredFieldError
 
         # ERROR=
-        error_label = node.get_value('ERROR')
+        error_label = node.get_value("ERROR")
         if error_label and self.is_error(node.label):
             return error_label
 
         # ACTION=VERIFY
-        if node.get_value('ACTION') == 'VERIFY':
+        if node.get_value("ACTION") == "VERIFY":
             data, _ = Pnr.get_pnr_data(self._get_pnr_locator(), key, item_number=1)
             if data is None:
-                not_found = node.get_value('NOTFOUND')
+                not_found = node.get_value("NOTFOUND")
                 if not_found is None:
                     raise PdredVerifyError
                 return not_found
@@ -74,8 +74,8 @@ class UserDefinedDbMacro(State):
         pd0_base = self._pd0_base(node)
 
         # Get the item number to read (Item numbers start from 1)
-        pd0_ctl_key: LabelReference = self.seg.lookup('PD0_CTL_KEY')
-        pd0_mc_cin: LabelReference = self.seg.lookup('PD0_MC_CIN')
+        pd0_ctl_key: LabelReference = self.seg.lookup("PD0_CTL_KEY")
+        pd0_mc_cin: LabelReference = self.seg.lookup("PD0_MC_CIN")
         last_key = self.vm.get_unsigned_value(pd0_base + pd0_ctl_key.dsp, pd0_ctl_key.length)
         item_number = self.vm.get_value(pd0_base + pd0_mc_cin.dsp, pd0_mc_cin.length) + 1 \
             if last_key == key_number else 1
@@ -84,39 +84,39 @@ class UserDefinedDbMacro(State):
         # SEARCH-n parameters
         starts_with: Optional[str] = None
         for index in range(1, 7):
-            search = node.get_value(f'SEARCH{index}')
+            search = node.get_value(f"SEARCH{index}")
             if search is None:
                 break
-            if search[0] == 'START':
-                if len(search) != 2 or not search[1].startswith("'") or not search[1].endswith("'"):
+            if search[0] == "START":
+                if len(search) != 2 or not search[1].startswith(""") or not search[1].endswith("""):
                     raise PdredSearchError
                 starts_with = search[1][1:-1]
 
         # Get the data
         pnr_locator = self._get_pnr_locator()
-        packed = node.get_value('FORMATOUT') == 'PACKED'
+        packed = node.get_value("FORMATOUT") == "PACKED"
         data, item_number = Pnr.get_pnr_data(pnr_locator, key, item_number, packed=packed, starts_with=starts_with)
         self.vm.set_value(item_number, pd0_base + pd0_mc_cin.dsp, pd0_mc_cin.length)
 
         # NOTFOUND & last item
         if data is None:
-            not_found = node.get_value('NOTFOUND')
+            not_found = node.get_value("NOTFOUND")
             if not_found is None:
                 raise PdredNotFoundError
             return not_found
         elif item_number == Pnr.get_len(pnr_locator, key):
-            last_item_bit: int = self.seg.evaluate('#PD0_RT_LST')
-            pd0_rt_id1: LabelReference = self.seg.lookup('PD0_RT_ID1')
+            last_item_bit: int = self.seg.evaluate("#PD0_RT_LST")
+            pd0_rt_id1: LabelReference = self.seg.lookup("PD0_RT_ID1")
             self.vm.or_bit(pd0_base + pd0_rt_id1.dsp, last_item_bit)
 
         # Update the data in PD0WRK
-        pd0_itm: LabelReference = self.seg.lookup('PD0_P_DATA') if packed else self.seg.lookup('PD0_C_ITM')
-        pd0_rt_adr: LabelReference = self.seg.lookup('PD0_RT_ADR')
+        pd0_itm: LabelReference = self.seg.lookup("PD0_P_DATA") if packed else self.seg.lookup("PD0_C_ITM")
+        pd0_rt_adr: LabelReference = self.seg.lookup("PD0_RT_ADR")
         if len(data) > pd0_itm.length:
             raise PdredPd0Error
         self.vm.set_bytes(data, pd0_base + pd0_itm.dsp, len(data))
         pd0_rt_adr_value = pd0_base + pd0_itm.dsp
-        if node.get_value('POINT') == 'YES':
+        if node.get_value("POINT") == "YES":
             attribute = Pnr.get_attribute_by_key(key)
             if packed:
                 pd0_rt_adr_value += (len(Pnr.HEADER) + len(attribute.std_fix))
@@ -133,8 +133,8 @@ class RealTimeDbMacro(State):
         address = self.vm.allocate()
         self._core_block(address, level)
         # Get file address and record id
-        record_id_address = self.get_ecb_address(level, 'CE1FA')
-        file_address_address = self.get_ecb_address(level, 'EBCFA')
+        record_id_address = self.get_ecb_address(level, "CE1FA")
+        file_address_address = self.get_ecb_address(level, "EBCFA")
         record_id = self.vm.get_unsigned_value(record_id_address, 2)
         file_address = self.vm.get_unsigned_value(file_address_address, 4)
         # Move the data in the work block
@@ -154,32 +154,36 @@ class RealTimeDbMacro(State):
 
 
 class TpfdfMacro(State):
-    C = {'E': '==', 'EQ': '==', 'NE': '!=', 'GE': '>=', 'LE': '<=', 'GT': '>', 'LT': '<', 'H': '>', 'L':  '<',
-         'NH': '<=', 'NL': '>='}
+    C = {"E": "==", "EQ": "==", "NE": "!=", "GE": ">=", "LE": "<=", "GT": ">", "LT": "<", "H": ">", "L": "<",
+         "NH": "<=", "NL": ">="}
 
     def _base_sw00sr(self) -> None:
         self.regs.R3 = self.vm.valid_address(self.regs.R3)
-        self.seg.load_macro('SW00SR')
+        self.seg.load_macro("SW00SR")
 
     def dbopn(self, node: KeyValue) -> str:
-        self.tpfdf_ref[node.get_value('REF')] = 0
+        self.tpfdf_ref[node.get_value("REF")] = 0
         return node.fall_down
 
     def dbcls(self, node: KeyValue) -> str:
-        if node.get_value('FILE') == 'PR001W':
+        if node.get_value("FILE") == "PR001W":
             return node.fall_down
-        del self.tpfdf_ref[node.get_value('REF')]
+        db_ref = node.get_value("REF")
+        if db_ref == "ALL":
+            self.tpfdf_ref.clear()
+        else:
+            del self.tpfdf_ref[db_ref]
         return node.fall_down
 
     def dbifb(self, node: KeyValue) -> str:
         self._base_sw00sr()
-        error_label = node.get_value('ERRORA')
-        if node.get_value('FILE') == 'PR001W':
+        error_label = node.get_value("ERRORA")
+        if node.get_value("FILE") == "PR001W":
             if error_label and self.is_error(node.label):
                 self.regs.R3 = 0
                 return error_label
             return node.fall_down
-        if node.get_value('REF') not in self.tpfdf_ref:
+        if node.get_value("REF") not in self.tpfdf_ref:
             self.regs.R3 = 0
             if error_label:
                 return error_label
@@ -189,51 +193,51 @@ class TpfdfMacro(State):
         self._base_sw00sr()
 
         # Setup KEY1 Primary key
-        pky = node.get_sub_value('KEY1', 'PKY')
+        pky = node.get_sub_value("KEY1", "PKY")
         if pky is not None:
-            key = f'{self.seg.evaluate(pky):02X}'
+            key = f"{self.seg.evaluate(pky):02X}"
         else:
             key = f"{self.seg.evaluate(node.get_sub_value('KEY1', 'S')):02X}"
 
         # Set up KEY2 to KEY6
         other_keys = dict()
         for key_number in range(2, 7):
-            key_n = f'KEY{key_number}'
+            key_n = f"KEY{key_number}"
             if node.get_value(key_n) is None:
                 break
-            df_field_name = node.get_sub_value(key_n, 'R')
-            condition = node.get_sub_value(key_n, 'C')
-            sign = self.C[condition] if condition is not None else '=='
-            field: FieldBaseDsp = node.get_sub_value(key_n, 'S')
+            df_field_name = node.get_sub_value(key_n, "R")
+            condition = node.get_sub_value(key_n, "C")
+            sign = self.C[condition] if condition is not None else "=="
+            field: FieldBaseDsp = node.get_sub_value(key_n, "S")
             if field is None:
                 # TODO For M, D, L types
                 raise DbredError
             base_address = self.regs.get_value(field.base)
             length = self.seg.lookup(df_field_name).length
             byte_array = self.vm.get_bytes(base_address + field.dsp, length)
-            other_keys[df_field_name] = (f'{sign} {byte_array}', length)
+            other_keys[df_field_name] = (f"{sign} {byte_array}", length)
 
         # Get the lrec
-        ref_name = node.get_value('REF')
-        item_number = 0 if node.get_value('BEGIN') else self.tpfdf_ref[ref_name]
+        ref_name = node.get_value("REF")
+        item_number = 0 if node.get_value("BEGIN") else self.tpfdf_ref[ref_name]
         item_number += 1
         lrec, item_number = Tpfdf.get_lrec(ref_name, key, item_number, other_keys)
         self.tpfdf_ref[ref_name] = item_number
 
         # Update error_code and REG=
         if lrec is None or self.is_error(node.label):
-            error_code = self.seg.evaluate('#TPFDBER')
+            error_code = self.seg.evaluate("#TPFDBER")
         else:
-            error_code = self.seg.evaluate('#TPFDBOK')
-            data_address = self.regs.R3 + self.seg.evaluate('SW00KL1')
+            error_code = self.seg.evaluate("#TPFDBOK")
+            data_address = self.regs.R3 + self.seg.evaluate("SW00KL1")
             self.vm.set_bytes(lrec, data_address, len(lrec))
-            reg = Register(node.get_value('REG'))
+            reg = Register(node.get_value("REG"))
             if reg.is_valid():
                 self.regs.set_value(data_address, reg)
-        self.vm.set_byte(error_code, self.regs.R3 + self.seg.evaluate('SW00RTN'))
+        self.vm.set_byte(error_code, self.regs.R3 + self.seg.evaluate("SW00RTN"))
 
         # ERROR=
-        error_label = node.get_value('ERRORA')
+        error_label = node.get_value("ERRORA")
         if error_label and self.is_error(node.label):
             return error_label
 
--- p8_test/test_local/test_eta1.py
+++ p8_test/test_local/test_eta1.py
@@ -1,5 +1,6 @@
 import unittest
 
+from p1_utils.data_type import DataType
 from p4_execution.ex5_execute import TpfServer
 from p8_test.test_local import TestDataUTS
 
@@ -10,9 +11,19 @@ class Eta1Test(unittest.TestCase):
         self.test_data = TestDataUTS()
         self.test_data.add_all_regs()
 
-    def test_eta1(self):
+    def test_eta1_vanilla(self):
         self.test_data.output.debug = ["ETA1"]
         test_data = self.tpf_server.run("ETA1", self.test_data)
         self.assertEqual("ETAX0000", test_data.output.last_line, test_data.output.last_node)
         self.assertNotIn("EXECUTION ERROR", test_data.output.messages)
         self.assertEqual(list(), test_data.output.dumps)
+
+    def test_eta1_el(self):
+        self.test_data.output.debug = ["ETA1"]
+        self.test_data.add_fields([("EBW000", 10)], "EB0EB")
+        self.test_data.set_field("MI0ACC", DataType("C", input="EL").to_bytes())
+        self.test_data.set_field("WA0FNS", DataType("X", input="10").to_bytes())
+        self.test_data.set_field("WA0UB4", DataType("X", input="08").to_bytes())
+        test_data = self.tpf_server.run("ETA1", self.test_data)
+        self.assertEqual("$$UIO1$$.1", test_data.output.last_line, test_data.output.last_node)
+        self.assertEqual("D9C5E2E3D9C9C3E3C5C4", test_data.get_field("EBW000"))  # RESTRICTED
