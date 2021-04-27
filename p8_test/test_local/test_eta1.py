4634811b965897c1243b659ae2e31c26e84c5c16
nayan <nayan@crazyideas.co.in>
2021-04-27 13:58:10 +0530

    Introducing test ETA1 execution.

---
+++ p8_test/test_local/test_eta1.py
@@ -0,0 +1,17 @@
+import unittest
+
+from p4_execution.ex5_execute import TpfServer
+from p8_test.test_local import TestDataUTS
+
+
+class Eta1Test(unittest.TestCase):
+    def setUp(self) -> None:
+        self.tpf_server = TpfServer()
+        self.test_data = TestDataUTS()
+        self.test_data.add_all_regs()
+
+    def test_eta1(self):
+        self.test_data.output.debug = ["ETA1"]
+        test_data = self.tpf_server.run("ETA1", self.test_data)
+        self.assertEqual("END", test_data.output.last_line, test_data.output.last_node)
+        self.assertNotIn("EXECUTION ERROR", test_data.output.messages)
