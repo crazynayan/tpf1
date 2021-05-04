d62865fbc8f378bd66089683d0d6fbeec39e65b6
nayan <nayan@crazyideas.co.in>
2021-05-04 11:59:38 +0530

    Include an exit stub for ETAZ.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 59
+    NUMBER_OF_FILES: int = 60
     SEG_NAME: str = "ETAF"
 
     def test_files(self):
