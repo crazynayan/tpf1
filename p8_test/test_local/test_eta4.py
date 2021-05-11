085f36ce547c244a0ca90271b19d548ab1e3d8d0
nayan <nayan@crazyideas.co.in>
2021-05-11 16:11:33 +0530

    ETA4 - test_eta4. Stub TNV0.

---
+++ p8_test/test_local/test_eta4.py
@@ -0,0 +1,50 @@
+import unittest
+
+from p1_utils.data_type import DataType
+from p3_db.pnr import RCVD_FROM, PHONE
+from p4_execution.ex5_execute import TpfServer
+from p8_test.test_local import TestDataUTS
+
+
+class Eta4Test(unittest.TestCase):
+    DEBUG_DATA = list()
+    SEGMENT = "ETA4"
+
+    def setUp(self) -> None:
+        self.tpf_server = TpfServer()
+        self.test_data = TestDataUTS()
+        self.test_data.output.debug = [self.SEGMENT]
+        self.output = None
+
+    def tearDown(self) -> None:
+        if not self.output or not self.output.debug:
+            return
+        for debug_line in self.output.debug:
+            if debug_line in self.DEBUG_DATA:
+                continue
+            self.DEBUG_DATA.append(debug_line)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print(f"{cls.SEGMENT} LOC = {len(cls.DEBUG_DATA)}")
+
+    def test_eta4_vanilla(self) -> None:
+        self.test_data.set_field("WA0ET5", DataType("X", input="01").to_bytes())
+        self.test_data.set_field("WA0ASC", DataType("X", input="01").to_bytes())
+        self.test_data.add_pnr_element(["NAYAN"], RCVD_FROM)
+        self.test_data.add_pnr_element(["123456"], PHONE)
+        test_data = self.tpf_server.run("ETA1", self.test_data)
+        self.assertEqual("ETA5420.9", test_data.output.last_line, test_data.output.last_node)
+        self.assertIn("NEED NAME IN PNR TO COMPLETE TRANSACTION", test_data.output.messages)
+        self.output = test_data.output
+
+    def test_eta4_tvl(self) -> None:
+        self.test_data.set_field("WA0ET5", DataType("X", input="01").to_bytes())
+        self.test_data.set_field("WA0ASC", DataType("X", input="01").to_bytes())
+        self.test_data.set_field("WA0FNS", DataType("X", input="10").to_bytes())
+        self.test_data.add_pnr_element(["NAYAN"], RCVD_FROM)
+        self.test_data.add_pnr_element(["123456"], PHONE)
+        test_data = self.tpf_server.run("ETA1", self.test_data)
+        self.assertEqual("ETA5420.9", test_data.output.last_line, test_data.output.last_node)
+        self.assertIn("NEED NAME IN PNR TO COMPLETE TRANSACTION", test_data.output.messages)
+        self.output = test_data.output
--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 65
+    NUMBER_OF_FILES: int = 66
     SEG_NAME: str = "ETA4"
 
     def test_files(self):
