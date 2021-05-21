76511f114f2d0a2b8bbd6f7d83070a12d7fd53db
nayan <nayan@crazyideas.co.in>
2021-05-21 16:15:45 +0530

    ETA6 - Added exit stub for etaa and backc stub for etam.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 72
+    NUMBER_OF_FILES: int = 74
     SEG_NAME: str = "ETA6"
 
     def test_files(self):
