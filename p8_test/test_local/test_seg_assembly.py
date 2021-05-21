54c544a8bf06efbd64d0552a713595894b462b32
nayan <nayan@crazyideas.co.in>
2021-05-21 15:04:19 +0530

    ETA6 - ETA6 assembly - Global @U1DMT

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -8,7 +8,7 @@ from p4_execution.ex5_execute import TpfServer
 
 class SegmentTest(unittest.TestCase):
     NUMBER_OF_FILES: int = 71
-    SEG_NAME: str = "ETAW"
+    SEG_NAME: str = "ETA6"
 
     def test_files(self):
         self.assertTrue("TS02" in segments)
