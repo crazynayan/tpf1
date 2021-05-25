edf057c7b57574a97312bc1e249c6aa8c28a1e09
nayan <nayan@crazyideas.co.in>
2021-05-25 10:35:13 +0530

    ETA6 - Added backc stub for ETAI, TTK4 and ETAD

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,8 +7,8 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 74
-    SEG_NAME: str = "ETA6"
+    NUMBER_OF_FILES: int = 77
+    SEG_NAME: str = "ETK2"
 
     def test_files(self):
         self.assertTrue("TS02" in segments)
