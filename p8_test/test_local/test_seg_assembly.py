2b9fca5aa383a63e33d7fd88ff9e3517e65587f3
nayan <nayan@crazyideas.co.in>
2021-05-31 10:31:01 +0530

    ETA9 - Assemble ETA9. Add @PTASW in globals.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -8,7 +8,7 @@ from p4_execution.ex5_execute import TpfServer
 
 class SegmentTest(unittest.TestCase):
     NUMBER_OF_FILES: int = 78
-    SEG_NAME: str = "ETAA"
+    SEG_NAME: str = "ETA9"
 
     def test_files(self):
         self.assertTrue("TS02" in segments)
