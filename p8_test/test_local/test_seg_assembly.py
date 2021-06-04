18646be42de78d16da847d19e91f60c81a354a0d
nayan <nayan@crazyideas.co.in>
2021-06-04 12:54:34 +0530

    ETG1 - Added stub for ULCA with error BACKC and MDAG stub with vanilla BACKC.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 83
+    NUMBER_OF_FILES: int = 85
     SEG_NAME: str = "ETG1"
 
     def test_files(self):
