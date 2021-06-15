c0b8bae0539c4e72cbe5bf853e02f440d135264c
nayan <nayan@crazyideas.co.in>
2021-06-15 16:08:03 +0530

    ETG2 - Added back stub for ETGG, WGB1. Added exitc stub for ETG3.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 90
+    NUMBER_OF_FILES: int = 93
     SEG_NAME: str = "ETG2"
 
     def test_files(self):
