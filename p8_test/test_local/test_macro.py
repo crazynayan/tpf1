985eed4a3f16d9518430c24bdf3fdbfe78aaafa1
nayan <nayan@crazyideas.co.in>
2021-06-18 14:23:34 +0530

    ETG3 - Added TQ6TQ, TC6TC. Update SUCCESS_END in TestDebug

--- p8_test/test_local/test_macro.py
+++ p8_test/test_local/test_macro.py
@@ -4,7 +4,7 @@ from p2_assembly.mac2_data_macro import DataMacro, macros
 
 
 class MacroTest(unittest.TestCase):
-    NUMBER_OF_FILES = 76
+    NUMBER_OF_FILES = 78
 
     def test_files(self):
         self.assertIn('EB0EB', macros)
@@ -286,9 +286,9 @@ class MacroTest(unittest.TestCase):
         for macro_name in macro_list:
             self._common_checks(macro_name)
 
-    def test_ETG1_ETG2_additions(self):
+    def test_ETG1_ETG2_ETG3_additions(self):
         macro_list = ["MH2MH", "P40P4", "TJ2TJ", "TRPCOM", "UI0OM", "UL0LC", "WIBBS", "TK1TK", "MI9MI", "JW1JW",
-                      "QR101W", "QP0QF", "TS2TS", "WKJWK", "TKITK", "SJ0EQ1"]
+                      "QR101W", "QP0QF", "TS2TS", "WKJWK", "TKITK", "SJ0EQ1", "TQ6TQ", "TC6TC"]
         for macro_name in macro_list:
             self._common_checks(macro_name)
 
