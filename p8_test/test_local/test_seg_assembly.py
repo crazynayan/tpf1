3264f2395a1069b77c3b4665604415811d6cb65a
nayan <nayan@crazyideas.co.in>
2021-06-18 14:21:14 +0530

    ETG3 - Added ETGE as exit stub.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 93
+    NUMBER_OF_FILES: int = 94
     SEG_NAME: str = "ETG3"
 
     def test_files(self):
