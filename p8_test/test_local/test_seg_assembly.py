f081cedfba087aba8910cc65f419787becc24a30
nayan <nayan@crazyideas.co.in>
2021-06-22 17:11:20 +0530

    EXAA - Segment assembly & execution.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,8 +7,8 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 96
-    SEG_NAME: str = "EXA1"
+    NUMBER_OF_FILES: int = 98
+    SEG_NAME: str = "EXAA"
 
     def test_files(self):
         self.assertTrue("TS02" in segments)
