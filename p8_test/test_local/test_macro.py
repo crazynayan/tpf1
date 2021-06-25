85e0d3f0cafe7e5574439459b63e226f1a064a97
nayan <nayan@crazyideas.co.in>
2021-06-25 12:10:12 +0530

    EXA1 - Added ECBEQ and CE1SW1 in EB0EB.

--- p8_test/test_local/test_macro.py
+++ p8_test/test_local/test_macro.py
@@ -4,7 +4,7 @@ from p2_assembly.mac2_data_macro import DataMacro, macros
 
 
 class MacroTest(unittest.TestCase):
-    NUMBER_OF_FILES = 93
+    NUMBER_OF_FILES = 94
 
     def test_files(self):
         self.assertIn('EB0EB', macros)
