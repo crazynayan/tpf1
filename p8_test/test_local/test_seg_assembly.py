b37c4ee6f99363dc6ab3c54c8600429bdf2b7cdd
nayan <nayan@crazyideas.co.in>
2021-06-29 15:24:41 +0530

    EXAK - Exit stub for EXA2.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 99
+    NUMBER_OF_FILES: int = 10
     SEG_NAME: str = "EXAK"
 
     def test_files(self):
