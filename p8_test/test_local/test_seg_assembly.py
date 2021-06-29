99c124bf66eb0737d857cf28ee97e250deb0dd93
nayan <nayan@crazyideas.co.in>
2021-06-29 15:26:15 +0530

    EXAK - 100 Count of segments

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 10
+    NUMBER_OF_FILES: int = 100
     SEG_NAME: str = "EXAK"
 
     def test_files(self):
