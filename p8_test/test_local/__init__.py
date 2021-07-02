96b4fe22e5335de555b8857d24f9969122759be5
nayan <nayan@crazyideas.co.in>
2021-07-02 10:18:19 +0530

    EXA2 - End at EXAN0000. Added exit stub for EXAA. Include EXA2 in TestDebug.

--- p8_test/test_local/__init__.py
+++ p8_test/test_local/__init__.py
@@ -62,8 +62,8 @@ class TestDataUTS(TestData):
 
 class TestDebug(unittest.TestCase):
     SEGMENTS = ["ETA1", "ETAX", "ETAF", "ETAZ", "ETK1", "ETKF", "ETA4", "ETA5", "ETAW", "ETA6", "ETK2", "ETK6", "ETAA",
-                "ETA9", "ETG1", "INS0", "ETG2", "ETGG", "ETG3", "ETGE", "EWA1", "EXA1", "EXAA", "EXAK"]
-    SUCCESS_END = "EXA20000"
+                "ETA9", "ETG1", "INS0", "ETG2", "ETGG", "ETG3", "ETGE", "EWA1", "EXA1", "EXAA", "EXAK", "EXA2"]
+    SUCCESS_END = "EXAN0000"
     ETG1_TJR_END = "ETG10750.2"
     EXAA_NPTY_END = "EXAA0525.6"
     FMSG_END = "FMSG0100"
--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 100
+    NUMBER_OF_FILES: int = 101
     SEG_NAME: str = "EXA2"
 
     def test_files(self):
