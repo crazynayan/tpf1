508c4f8e435c87f042b10132090c93d75c25581f
nayan <nayan@crazyideas.co.in>
2021-07-13 14:17:27 +0530

    EXA5 - Added PD0CL and AP0CI data macro.

--- p8_test/test_local/test_macro.py
+++ p8_test/test_local/test_macro.py
@@ -4,7 +4,7 @@ from p2_assembly.mac2_data_macro import DataMacro, macros
 
 
 class MacroTest(unittest.TestCase):
-    NUMBER_OF_FILES = 100
+    NUMBER_OF_FILES = 102
 
     def test_files(self):
         self.assertIn('EB0EB', macros)
@@ -298,8 +298,8 @@ class MacroTest(unittest.TestCase):
         for macro_name in macro_list:
             self._common_checks(macro_name)
 
-    def test_EXA_EXA4_additions(self):
-        macro_list = ["PD0CTL", "GW01SR", "PD0EWK", "WB1WB"]
+    def test_EXA_EXA4_EXA5_additions(self):
+        macro_list = ["PD0CTL", "GW01SR", "PD0EWK", "WB1WB", "PD0CL", "AP0CI"]
         for macro_name in macro_list:
             self._common_checks(macro_name)
 
