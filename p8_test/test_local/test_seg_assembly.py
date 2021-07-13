347cd6379e1d7df05187045b1d94b79facd9b598
nayan <nayan@crazyideas.co.in>
2021-07-13 14:57:35 +0530

    EXA5 - Exit stub for EXE1

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,8 +7,8 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 114
-    SEG_NAME: str = "EXA4"
+    NUMBER_OF_FILES: int = 115
+    SEG_NAME: str = "EXA5"
 
     def test_files(self):
         self.assertTrue("TS02" in segments)
