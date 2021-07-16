cb7c68b8bdfb8ae519f82a1d7f06fb15e80cf97a
nayan <nayan@crazyideas.co.in>
2021-07-16 11:50:49 +0530

    EXE1 - Added support for ADM_CLUB in PDRED.

--- p3_db/pnr.py
+++ p3_db/pnr.py
@@ -8,7 +8,7 @@ from p3_db.stream import Stream
 
 NAME, HFAX, FQTV, ITIN, RCVD_FROM, PHONE, REMARKS = "name", "hfax", "fqtv", "itin", "rcvd_from", "phone", "remarks"
 SUBS_CARD_SEG, GROUP_PLAN, RECORD_LOC, PRS_SEATS = "subs_card_seg", "group_plan", "record_loc", "prs_seats"
-HEADER, VCR_COUPON, ICE_DATA, GFAX = "header", "vcr_coupon", "ice_data", "gfax"
+HEADER, VCR_COUPON, ICE_DATA, GFAX, ADM_CLUB = "header", "vcr_coupon", "ice_data", "gfax", "adm_club"
 
 
 class PnrAttribute:
@@ -64,6 +64,7 @@ class Pnr:
         PnrAttribute(HEADER, "20", byte_array=True, packed=True),
         PnrAttribute(VCR_COUPON, "64", std_var=bytearray([0x80])),  # Indicate NEW item
         PnrAttribute(ICE_DATA, "38", std_var=bytearray([0x80])),  # Indicate NEW item
+        PnrAttribute(ADM_CLUB, "82", std_var=bytearray([0x80])),  # Indicate NEW item
     ]
 
     @classmethod
