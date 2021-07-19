6d6aa369329c87550f3c2cd8042f835f44158847
nayan <nayan@crazyideas.co.in>
2021-07-19 10:24:34 +0530

    EXE1 - Test ET2 direct will now end at SUCCESS_END.

--- p8_test/test_local/test_exaa.py
+++ p8_test/test_local/test_exaa.py
@@ -80,8 +80,10 @@ class ExaaTest(TestDebug):
     def test_et2_direct(self) -> None:
         self._setup_output_pnr_header()
         self.test_data.set_field("WA0PTY", DataType("X", input="03").to_bytes())
+        self.test_data.set_field("WA0POR", DataType("X", input="00017F").to_bytes())
+        self.test_data.set_field("WA0ASC", DataType("X", input="01").to_bytes())
         self.test_data.add_pnr_element(["3ZAVERI/NAYAN/PURVI/SANAY"], NAME)
         test_data = self.tpf_server.run("EWA1", self.test_data)
         self.output = test_data.output
-        self.assertEqual(self.IGR1_END, self.output.last_line, f"{self.output.last_node}--{self.output.dumps}")
+        self.assertEqual(self.SUCCESS_END, self.output.last_line, f"{self.output.last_node}--{self.output.dumps}")
         self.assertEqual("0003", test_data.get_pnr_field("PR00_20_PTY"))
