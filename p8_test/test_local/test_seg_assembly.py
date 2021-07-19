f9444dbfca33d4a4fe10ce85945e1d634415da7f
nayan <nayan@crazyideas.co.in>
2021-07-19 11:39:10 +0530

    EXE2 - Now SUCCESS_END is at IGR1 to make test cases work.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -8,7 +8,7 @@ from p4_execution.ex5_execute import TpfServer
 
 class SegmentTest(unittest.TestCase):
     NUMBER_OF_FILES: int = 120
-    SEG_NAME: str = "EXE1"
+    SEG_NAME: str = "EXE2"
 
     def test_files(self):
         self.assertTrue("TS02" in segments)
