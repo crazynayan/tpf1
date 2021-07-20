f026588fb57a424f719c11352b7eb8031c9ca98b
nayan <nayan@crazyideas.co.in>
2021-07-20 10:30:31 +0530

    EXE2 - Test with Itin

--- p8_test/test_local/test_et2.py
+++ p8_test/test_local/test_et2.py
@@ -1,5 +1,7 @@
+from base64 import b64encode
+
 from p1_utils.data_type import DataType
-from p3_db.pnr import NAME, HEADER
+from p3_db.pnr import NAME, HEADER, ITIN
 from p8_test.test_local import TestDebug
 
 
@@ -16,7 +18,16 @@ class Et2Test(TestDebug):
         }
         self.test_data.create_pnr_output(pnr_output_dict=pnr_dict, persistence=False)
 
-    def test_et2_direct(self) -> None:
+    def _setup_input_itin(self) -> None:
+        itin_input = [
+            {
+                "WI0SZE": b64encode(DataType("X", input="F0").to_bytes()).decode(),
+                "WI0ARC": b64encode(DataType("C", input="BA").to_bytes()).decode(),
+            }
+        ]
+        self.test_data.add_pnr_field_data(itin_input, ITIN)
+
+    def test_without_itin(self) -> None:
         self._setup_output_pnr_header()
         self.test_data.set_field("WA0PTY", DataType("X", input="03").to_bytes())
         self.test_data.set_field("WA0POR", DataType("X", input="00017F").to_bytes())
@@ -25,4 +36,16 @@ class Et2Test(TestDebug):
         test_data = self.tpf_server.run("EWA1", self.test_data)
         self.output = test_data.output
         self.assertEqual(self.IGR1_END, self.output.last_line, f"{self.output.last_node}--{self.output.dumps}")
+        self.assertEqual("0003", test_data.get_pnr_field("PR00_20_PTY"))
+
+    def test_with_itin(self) -> None:
+        self._setup_output_pnr_header()
+        self.test_data.set_field("WA0PTY", DataType("X", input="03").to_bytes())
+        self.test_data.set_field("WA0POR", DataType("X", input="00017F").to_bytes())
+        self.test_data.set_field("WA0ASC", DataType("X", input="01").to_bytes())
+        self.test_data.add_pnr_element(["3ZAVERI/NAYAN/PURVI/SANAY"], NAME)
+        self._setup_input_itin()
+        test_data = self.tpf_server.run("EWA1", self.test_data)
+        self.output = test_data.output
+        self.assertEqual(self.SUCCESS_END, self.output.last_line, f"{self.output.last_node}--{self.output.dumps}")
         self.assertEqual("0003", test_data.get_pnr_field("PR00_20_PTY"))
