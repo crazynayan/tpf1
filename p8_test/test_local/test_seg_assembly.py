1729723f2b4c6060cd036a582576357fcfc02af3
nayan <nayan@crazyideas.co.in>
2021-07-20 10:36:59 +0530

    EXE2 - Exit stub for EXER

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -7,7 +7,7 @@ from p4_execution.ex5_execute import TpfServer
 
 
 class SegmentTest(unittest.TestCase):
-    NUMBER_OF_FILES: int = 120
+    NUMBER_OF_FILES: int = 121
     SEG_NAME: str = "EXE2"
 
     def test_files(self):
