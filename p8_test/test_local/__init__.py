b723ed6493508f9a000a059e4296966f39d39474
nayan <nayan@crazyideas.co.in>
2021-07-21 12:23:56 +0530

    EXE6 - Added TestDebug for EXE6.

--- p8_test/test_local/__init__.py
+++ p8_test/test_local/__init__.py
@@ -63,8 +63,8 @@ class TestDataUTS(TestData):
 class TestDebug(unittest.TestCase):
     SEGMENTS = ["ETA1", "ETAX", "ETAF", "ETAZ", "ETK1", "ETKF", "ETA4", "ETA5", "ETAW", "ETA6", "ETK2", "ETK6", "ETAA",
                 "ETA9", "ETG1", "INS0", "ETG2", "ETGG", "ETG3", "ETGE", "EWA1", "EXA1", "EXAA", "EXAK", "EXA2", "EXA3",
-                "EXA8", "EXA9", "EXA4", "EXA5", "EXE1", "EXE2", "EXER", "EXE3"]
-    SUCCESS_END = "EXE60000"
+                "EXA8", "EXA9", "EXA4", "EXA5", "EXE1", "EXE2", "EXER", "EXE3", "EXE6"]
+    SUCCESS_END = "EXE40000"
     ETG1_TJR_END = "ETG10750.2"
     EXAA_NPTY_END = "EXAA0525.6"
     FMSG_END = "FMSG0100"
