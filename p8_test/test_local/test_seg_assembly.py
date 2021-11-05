a5f81b410534502bb6615c8e11e3b9f5d6a0641b
nayan <nayan@crazyideas.co.in>
2021-11-05 09:31:56 +0530

    Listing v2 - test_seg_assembly - Check for Assembly error.

--- p8_test/test_local/test_seg_assembly.py
+++ p8_test/test_local/test_seg_assembly.py
@@ -1,5 +1,6 @@
 import unittest
 
+from p1_utils.errors import AssemblyError
 from p2_assembly.seg6_segment import seg_collection, Segment
 from p4_execution.ex5_execute import TpfServer
 
@@ -10,9 +11,17 @@ class SegmentTest(unittest.TestCase):
     def test_segment(self):
         self.maxDiff = None
         seg: Segment = seg_collection.get_seg(self.SEG_NAME)
-        seg.assemble()
-        unknown = [str(node) for _, node in seg.nodes.items() if node.command not in TpfServer().supported_commands]
-        self.assertListEqual(list(), unknown, "\nUnknown in execute.")
+        try:
+            seg.assemble()
+        except AssemblyError:
+            self.assertFalse(True, msg=seg.error_line)
+        unknown = [str(node)[:60] for _, node in seg.nodes.items() if
+                   node.command not in TpfServer().supported_commands]
+        # all_nodes = [str(node) for _, node in seg.nodes.items()]
+        # all_nodes.sort()
+        # with open(f"tmp/{self.SEG_NAME}-assemble.txt", "w") as fh:
+        #     fh.writelines(f"{node}\n" for node in all_nodes)
+        self.assertListEqual(list(), unknown, "\nUnsupported Instruction.")
 
 
 if __name__ == "__main__":
