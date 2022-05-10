b3e723e8a23f24c699f90448f0d16c1641b6a5c5
nayan <nayan@crazyideas.co.in>
2022-05-10 13:58:13 +0530

    Added td_elements in cli init.

--- p7_flask_app/__init__.py
+++ p7_flask_app/__init__.py
@@ -24,7 +24,8 @@ def make_shell_context():
     from p2_assembly.seg8_listing import create_listing_commands, write_tmp_output, LstCmd
     from p2_assembly.seg9_collection import SegLst, seg_collection
     from p3_db.test_data import TestData
-    from p3_db import template_crud, template_merge, test_data_get, template_models, test_data_variations
+    from p3_db import template_crud, template_merge, test_data_get, template_models, test_data_variations, \
+        test_data_elements
     from p4_execution.ex5_execute import TpfServer
     from p7_flask_app.auth import User
     from p7_flask_app import routes
@@ -48,4 +49,5 @@ def make_shell_context():
         "test_data_get": test_data_get,
         "templates": template_models,
         "td_variations": test_data_variations,
+        "td_elements": test_data_elements,
     }
