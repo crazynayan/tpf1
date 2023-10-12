f6fa21a4506856c27211414892273cf0eeef61a7
nayan <nayan@crazyideas.co.in>
2023-10-12 11:46:21 +0530

    Backend - TestAPI: Ensure multiple domain authentication in one run. Use token cache.

--- p8_test/test_api/__init__.py
+++ p8_test/test_api/__init__.py
@@ -2,25 +2,24 @@ from typing import Callable
 
 from munch import Munch
 
-from config import config
 from p7_flask_app import tpf1_app
 from p7_flask_app.auth import User
 
 CLIENT = tpf1_app.test_client()
 
-
+TOKEN_CACHE: Munch = Munch()
 def authorized_request(func: Callable, url: str, email: str, **kwargs) -> Munch:
     if "api_other_auth" in kwargs:
         token = User.objects.filter_by(email="john.stack@smltd.com").first().token
         del kwargs["api_other_auth"]
     else:
-        if not config.TEST_TOKEN:
-            config.TEST_TOKEN = User.objects.filter_by(email=email).first().token
-        token = config.TEST_TOKEN
+        if email not in TOKEN_CACHE:
+            TOKEN_CACHE[email] = User.objects.filter_by(email=email).first().token
+        token = TOKEN_CACHE[email]
     kwargs['headers'] = {'Authorization': f"Bearer {token}"}
     response = func(url, **kwargs)
-    if response.status_code == 401:
-        config.TEST_TOKEN = str()
+    if response.status_code == 401 and email in TOKEN_CACHE:
+        del TOKEN_CACHE[email]
     if response.status_code != 200:
         return Munch()
     return Munch.fromDict(response.get_json())
