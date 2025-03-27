9fd20a6da1df3858b9345164861a7d19e7696faf
KChaturvedi09 <komal.chaturvedi1610@gmail.com>
2025-03-27 14:56:56 +0530

    Test case for carrier code translation (CCT).

---
+++ d21_backend/p8_test/test_local/test_execution/test_trg1.py
@@ -0,0 +1,148 @@
+import unittest
+from base64 import b64encode
+from copy import deepcopy
+
+from d21_backend.config import config
+from d21_backend.p1_utils.data_type import DataType
+from d21_backend.p2_assembly.mac2_data_macro import get_macros
+from d21_backend.p3_db.test_data_elements import FixedFile, PoolFile, FileItem
+from d21_backend.p4_execution.ex5_execute import TpfServer
+from d21_backend.p8_test.test_local import TestDataUTS
+
+
+class CctTest(unittest.TestCase):
+    tpf_server: TpfServer = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.tpf_server = TpfServer()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        return
+
+    def setUp(self) -> None:
+        self.tpf_server.init_run("TRG1")
+        self.test_data = TestDataUTS()
+        # Test data display fields
+        self.test_data.add_fields([("EBW000",2),("EBX00",2),("EBT000",2)],"EB0EB")
+        # Item setup
+        self.t4_item = [{"field": "T40CCD", "data": b64encode(bytearray[0xC1, 0xC1]).decode()}]
+        self.u4_item = [{"field": "U49CAR", "data": b64encode(bytearray[0xC1, 0xC1]).decode()},
+                        {"field": "U49NAM", "data": b64encode(bytearray[0xC1, 0xD4, 0xC5, 0xD9, 0xC9, 0xC3, 0xC1, 0xD5, 0x40, 0xC1, 0xC9, 0xD9, 0xD3, 0xC9, 0xD5, 0xC5, 0xE2]).decode()}]
+
+    def _tjr_setup(self, iy_chain_count: int = 0, variation: int = 0):
+        # Fixed File setup
+        fixed_file = FixedFile()
+        fixed_file.variation = variation
+        fixed_file.rec_id = DataType("C", input="T4").value
+        fixed_file.macro_name = "T404T"
+        fixed_file.fixed_type = get_macros()["SYSEQC"].evaluate("#MISCL")
+        fixed_file.fixed_ordinal = 0x17F
+        item = FileItem()
+        fixed_file.file_items.append(item)
+        item.macro_name = "T404T"
+        item.field = "T40ITM"
+        item.field_data = deepcopy(self.t4_item)
+        item.count_field = "T40NBR"
+        item.adjust = True
+        # Pool File setup
+        pool_file = PoolFile()
+        fixed_file.pool_files.append(pool_file)
+        pool_file.macro_name = "U404U"
+        pool_file.rec_id = DataType("C", input="U4").value
+        pool_file.index_macro_name = "T404T"
+        pool_file.index_field = "TJ0ATH"
+        pool_file.forward_chain_label = "T40FAD"
+        pool_file.forward_chain_count = u4_chain_count
+        item = FileItem()
+        pool_file.file_items.append(item)
+        item.macro_name = "U404U"
+        item.field = "U40ITM"
+        item.field_data = deepcopy(self.u4_item)
+        item.count_field = "U40NBR"
+        item.adjust = False
+        file_dict = fixed_file.cascade_to_dict()
+        del file_dict["id"]
+        del file_dict["pool_files"][0]["id"]
+        del file_dict["pool_files"][0]["file_items"][0]["id"]
+        self.test_data.create_fixed_file(file_dict, persistence=False)
+        return
+
+    def test_cct01_carrier_code_to_airline_name_translation(self) -> None:
+        # Test data setup
+        self.test_data.set_global_field("@TRROR", "0000006E", 0)
+        self.test_data.set_global_field("@TRROR", "0000006E", 1)
+        self.test_data.set_global_field("@TRROR", "0000006E", 2)
+        self.test_data.set_global_field("@TRROR", "0000006E", 3)
+        self.test_data.create_ecb_level({"variation": 0, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C161C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 1, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C161C2C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 2, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C2C161C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 3, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C2C161C2C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self._tjr_setup
+        test_data = self.tpf_server.run("TRG1", self.test_data)
+        self.assertIn("AMERICAN AIRLIN", test_data.output[0].messages)
+        self.assertIn("CARRIER CODE NOT PRESENT", test_data.output[1].messages)
+        self.assertIn("TRAINEE CODE NOT PRESENT", test_data.output[2].messages)
+        self.assertIn("TRAINEE CODE NOT PRESENT", test_data.output[3].messages)
+
+    def test_cct02_input_entry_validation(self) -> None:
+        self.test_data.create_ecb_level({"variation": 0, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C161C1C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 1, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D76161C1C161C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 2, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C1C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 3, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161F1C161C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 4, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1F161C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 5, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C161F1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self.test_data.create_ecb_level({"variation": 6, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C161C1F14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self._tjr_setup()
+        test_data = self.tpf_server.run("TRG1", self.test_data)
+        self.assertIn("INVALID CARRIER CODE", test_data.output[0].messages)
+        self.assertIn("INVALID ACTION CODE", test_data.output[1].messages)
+        self.assertIn("INVALID SEPARATOR", test_data.output[2].messages)
+        self.assertIn("TRAINEE CODE NOT ALPHA", test_data.output[3].messages)
+        self.assertIn("TRAINEE CODE NOT ALPHA", test_data.output[4].messages)
+        self.assertIn("CARRIER CODE NOT ALPHA", test_data.output[5].messages)
+        self.assertIn("CARRIER CODE NOT ALPHA", test_data.output[6].messages)
+
+    def test_cct03_file_error(self) -> None:
+        # Test data setup
+        self.test_data.set_global_field("@TRROR", "00000000", 0)
+        self.test_data.create_ecb_level({"variation": 0, "variation_name": str(), "ecb_level": "0", "hex_data": "0000D96161C1C161C1C14E",
+                                         "field_data": str(), "seg_name": str()}, persistence=False)
+        self._tjr_setup
+        test_data = self.tpf_server.run("TRG1", self.test_data
+        self.assertIn("CAD002", test_data.output[0].dumps)
+
+    def test_finwc_fail(self) -> None:
+        self.iy_item[0]["data"] = b64encode(DataType("X", input="00006F2F").to_bytes()).decode()
+        self._tjr_setup()
+        self.test_data.errors.append("TSJ1FINE")
+        test_data = self.tpf_server.run("TRG1", self.test_data)
+        self.assertEqual("TSJ1500.1", test_data.output.last_line)
+        self.assertIn("BAD123", test_data.output.dumps)
+
+    def test_variation(self):
+        self.iy_item[0]["data"] = b64encode(DataType("X", input="00006F2F").to_bytes()).decode()
+        self._tjr_setup()
+        self.iy_item[1]["data"] = b64encode(bytearray([get_macros()["IY1IY"].evaluate("#IY1LOK")])).decode()
+        self._tjr_setup(variation=1)
+        test_data = self.tpf_server.run("TSJ1", self.test_data)
+        self.assertEqual("TSJ1ERR.3", test_data.outputs[0].last_line)
+        self.assertEqual(8, test_data.outputs[0].regs["R6"])
+        self.assertEqual("TSJ1PASS.3", test_data.outputs[1].last_line)
+        self.assertEqual(0, test_data.outputs[1].regs["R6"])
+
+
+if __name__ == "__main__":
+    unittest.main()
